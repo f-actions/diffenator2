5e3e1a786ee3138e6264c6e0d346a9230ee49796
Marc Foley <m.foley.88@gmail.com>
2021-10-08 14:24:10 +0100

    tester

--- test.py
+++ test.py
@@ -1,31 +1,32 @@
-"""
-Script is too complicated to be a simple bash script.
-"""
-import os
-from fontTools.ttLib import TTFont
-from gftools.html import HtmlProof, HtmlDiff
-from gftools.utils import font_familyname, download_family_from_Google_Fonts
-from glob import glob
-import argparse
-
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument("path")
-parser.add_argument("--pt_size", default=16)
-parser.add_argument("--fonts_before")
-args = parser.parse_args()
-
-
-fonts = glob(os.path.join(args.path, "*.ttf"))
-
-# User just wants proofs
-#if not args.fonts_before:
-html = HtmlProof(
-    fonts,
-    out="screenshots",
-    selenium_screenshots=True
-)
+print("hello world")
+#"""
+#Script is too complicated to be a simple bash script.
+#"""
+#import os
+#from fontTools.ttLib import TTFont
+#from gftools.html import HtmlProof, HtmlDiff
+#from gftools.utils import font_familyname, download_family_from_Google_Fonts
+#from glob import glob
+#import argparse
+#
+#
+#
+#parser = argparse.ArgumentParser()
+#parser.add_argument("path")
+#parser.add_argument("--pt_size", default=16)
+#parser.add_argument("--fonts_before")
+#args = parser.parse_args()
+#
+#
+#fonts = glob(os.path.join(args.path, "*.ttf"))
+#
+## User just wants proofs
+##if not args.fonts_before:
+#html = HtmlProof(
+#fonts,
+#out="screenshots",
+#selenium_screenshots=True
+#)
 #else:
 #    # User wants to diff against Google Fonts
 #    ttFont = TTFont(fonts[0])
@@ -43,7 +44,7 @@ html = HtmlProof(
 #        out="screenshots",
 #        selenium_screenshots=True
 #    )
-
-
-html.build_pages(pt_size=args.pt_size)
-html.save_imgs()
+#
+#
+#html.build_pages(pt_size=args.pt_size)
+#html.save_imgs()
