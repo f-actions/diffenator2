97e1d57336150b253f8f8cf126a3bcd104603801
Marc Foley <m.foley.88@gmail.com>
2021-10-08 14:19:59 +0100

    test just proofs fow now

--- test.py
+++ test.py
@@ -20,29 +20,29 @@ args = parser.parse_args()
 fonts = glob(os.path.join(args.path, "*.ttf"))
 
 # User just wants proofs
-if not args.fonts_before:
-    html = HtmlProof(
-        fonts,
-        out="screenshots",
-        selenium_screenshots=True
-    )
-else:
-    # User wants to diff against Google Fonts
-    ttFont = TTFont(fonts[0])
-    family_name = font_familyname(ttFont)
-    if args.fonts_before == "google-fonts":
-        os.mkdir("fonts_before")
-        fonts_before = download_family_from_Google_Fonts(
-            family_name,
-            "fonts_before"
-        )
-    # TODO add more font_before inputs
-    html = HtmlDiff(
-        fonts_before,
-        fonts,
-        out="screenshots",
-        selenium_screenshots=True
-    )
+#if not args.fonts_before:
+html = HtmlProof(
+    fonts,
+    out="screenshots",
+    selenium_screenshots=True
+)
+#else:
+#    # User wants to diff against Google Fonts
+#    ttFont = TTFont(fonts[0])
+#    family_name = font_familyname(ttFont)
+#    if args.fonts_before == "google-fonts":
+#        os.mkdir("fonts_before")
+#        fonts_before = download_family_from_Google_Fonts(
+#            family_name,
+#            "fonts_before"
+#        )
+#    # TODO add more font_before inputs
+#    html = HtmlDiff(
+#        fonts_before,
+#        fonts,
+#        out="screenshots",
+#        selenium_screenshots=True
+#    )
 
 
 html.build_pages(pt_size=args.pt_size)
