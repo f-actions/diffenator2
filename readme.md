136cc07c7be1117a03d400d784e45698792ec5df
Marc Foley <m.foley.88@gmail.com>
2021-10-12 16:53:54 +0100

    refine

--- readme.md
+++ readme.md
@@ -1,12 +1,12 @@
-# Font Browser Tests Action
+# Diffbrowsers Action
 
-Test how your fonts render on different browsers
+Test how fonts render on different browsers using `gftools gen-html`
 
-## Example
+## Usage
 
 Create a `.github/workflows/test.yml` with the following contents:
 
-```
+```YAML
 on: [push]
 
 jobs:
@@ -15,16 +15,51 @@ jobs:
     name: Check fonts on different browsers
     steps:
         - uses: actions/checkout@v2
-        - uses: m4rc1e/font-browser-tests-action
+        - uses: m4rc1e/diffbrowsers-action@latest
+          with:
+            path: ./fonts # Path to a directory of fonts
+            pt_size: 15 # Change text pt size in html docs (Optional) 
+            fonts_before: 'google-fonts' # Diff against previous fonts (Optional)
 ```
-
 
 Want to test different operating systems? use a matrix.
 
-```
-TODO
+```YAML
+name: Test screenshots
+
+on:
+  push:
+    branches: [main]
+
+jobs:
+  screenshot:
+    runs-on: ${{ matrix.os }}
+    strategy:
+      matrix:
+        python-version: [3.9]
+        os: [ubuntu-latest, windows-latest, macos-latest]
+    steps:
+      - uses: actions/checkout@v2
+        with:
+          submodules: recursive
+          fetch-depth: 0
+      - uses: m4rc1e/diffbrowsers-action@v0.0.34
+        with:
+          path: ./fonts
+          pt_size: 15
+          fonts_before: google-fonts
+          width: 1280
 ```
 
-## Inputs
 
-You can further customise the action using the following inputs:
+Use `upload-artifact` to save the images
+
+```YAML
+- name: Create Artifacts
+  uses: actions/upload-artifact@v2
+  with:
+    name: images
+    path: ./screenshots/img
+```
+
+
--- test.py
+++ test.py
@@ -12,40 +12,42 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("path")
+parser.add_argument("--paths", nargs="+", required=True)
 parser.add_argument("--pt_size", default=16)
-parser.add_argument("--fonts_before")
+parser.add_argument("--fonts_before", default="none")
 parser.add_argument("--width", type=int, default=1280)
+parser.add_argument("--out", default="screenshots")
 args = parser.parse_args()
 
+os.mkdir(args.out)
 
-fonts = glob(os.path.join(args.path, "*.ttf"))
-
-# User just wants proofs
-if args.fonts_before == "none":
-    html = HtmlProof(
-        fonts,
-        out="screenshots",
-        selenium_screenshots=True
-    )
-else:
-    # User wants to diff against Google Fonts
+for font_dir in args.paths:
+    fonts = glob(os.path.join(font_dir, "*.ttf"))
     ttFont = TTFont(fonts[0])
     family_name = font_familyname(ttFont)
-    if args.fonts_before == "google-fonts":
-        os.mkdir("fonts_before")
-        fonts_before = download_family_from_Google_Fonts(
-            family_name,
-            "fonts_before"
+    out = os.path.join(args.out, family_name)
+
+    # User just wants proofs
+    if args.fonts_before == "none":
+        html = HtmlProof(
+            fonts,
+            out=out,
+            selenium_screenshots=True
         )
-    # TODO add more font_before inputs
-    html = HtmlDiff(
-        fonts_before,
-        fonts,
-        out="screenshots",
-        selenium_screenshots=True
-    )
+    else:
+        # User wants to diff against Google Fonts
+        if args.fonts_before == "google-fonts":
+            os.mkdir("fonts_before")
+            fonts_before = download_family_from_Google_Fonts(
+                family_name,
+                "fonts_before"
+            )
+        html = HtmlDiff(
+            fonts_before,
+            fonts,
+            out=out,
+            selenium_screenshots=True
+        )
 
-
-html.build_pages(pt_size=args.pt_size)
-html.save_imgs(width=args.width)
+    html.build_pages(pt_size=args.pt_size)
+    html.save_imgs(width=args.width)
