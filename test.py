d119a7e08d211bcf6378191ee9a4cbea79933ad2
Marc Foley <m.foley.88@gmail.com>
2022-09-29 10:39:03 +0100

    use diffenator2 alpha

--- test.py
+++ test.py
@@ -1,59 +1,54 @@
 #!/usr/bin/env python3
 """
-Script is too complicated to be a simple bash script.
 """
 import os
 from fontTools.ttLib import TTFont
-from gftools.html import HtmlProof, HtmlDiff
-from gftools.utils import font_familyname, download_family_from_Google_Fonts
 from glob import glob
 import argparse
-from pkg_resources import resource_filename
+from diffenator import run_proofing_tools, run_diffing_tools
+from diffenator.utils import (
+    download_googlefonts_release_archive,
+    download_latest_github_release_archive,
+)
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--paths", nargs="+", required=True)
-parser.add_argument("--pt_size", default=16)
 parser.add_argument("--fonts_before", default="none")
-parser.add_argument("--width", type=int, default=1280)
-parser.add_argument("--out", default="screenshots")
-parser.add_argument("--template_dir", default="none")
+parser.add_argument("--out", default=os.path.abspath("screenshots"))
 args = parser.parse_args()
 
 
-template_dir = args.template_dir if args.template_dir != "none" else resource_filename("gftools", "templates")
-
 os.mkdir(args.out)
+
 
 for font_dir in args.paths:
     fonts = glob(os.path.join(font_dir, "*.ttf"))
-    ttFont = TTFont(fonts[0])
-    family_name = font_familyname(ttFont)
+    ttFonts = [TTFont(f) for f in fonts]
+    family_name = ttFonts[0]["name"].getBestFamilyName()
     out = os.path.join(args.out, family_name)
 
     # User just wants proofs
     if args.fonts_before == "none":
-        html = HtmlProof(
-            fonts,
-            out=out,
-            template_dir=template_dir,
-            selenium_screenshots=True,
-        )
+        run_proofing_tools(ttFonts, out=out, imgs=True)
     else:
         # User wants to diff against Google Fonts
+        fonts_before_fp = "fonts_before"
         if args.fonts_before == "google-fonts":
-            os.mkdir("fonts_before")
-            fonts_before = download_family_from_Google_Fonts(
-                family_name,
-                "fonts_before"
+            gf_archive = download_googlefonts_release_archive(
+                family_name, "fonts_before"
             )
-        html = HtmlDiff(
-            fonts_before,
-            fonts,
-            out=out,
-            template_dir=template_dir,
-            selenium_screenshots=True
-        )
-
-    html.build_pages(pt_size=args.pt_size)
-    html.save_imgs(width=args.width)
+        else:
+            user, repo = args.fonts_before.split("/")
+            gf_archive = download_latest_github_release_archive(
+                user,
+                repo,
+                fonts_before_fp,
+                os.environ["GITHUB_TOKEN"] or os.environ["GH_TOKEN"],
+            )
+        ttFonts_before = [
+            TTFont(f) for f in glob(os.path.join(fonts_before_fp, "*.ttf"))
+        ]
+        import pdb
+        pdb.set_trace()
+        run_diffing_tools(ttFonts_before, ttFonts, out=out, imgs=True)
