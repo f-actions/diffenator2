308708dbd6b4546f5cff4c63c3ccfa4d7ea1032f
Marc Foley <m.foley.88@gmail.com>
2022-10-07 11:53:35 +0100

    use none vals

--- test.py
+++ test.py
@@ -17,10 +17,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--github-token", required=True)
 parser.add_argument("--path", required=True, nargs="+")
 
-parser.add_argument("--fetch-before", choices=("", "googlefonts", "github-release"), default="")
-parser.add_argument("--repo", default="")
+parser.add_argument("--fetch-before", choices=("none", "googlefonts", "github-release"), default="none")
+parser.add_argument("--repo", default="none")
 
-parser.add_argument("--path-before", default="")
+parser.add_argument("--path-before", default="none")
 
 parser.add_argument("--out", default="screenshots")
 args = parser.parse_args()
@@ -36,18 +36,18 @@ family_name = ttFonts[0]["name"].getBestFamilyName()
 out = os.path.join(out, family_name.replace(" ", "-"))
 
 # User just wants proofs
-if not args.path_before:
+if args.path_before != "none":
     run_proofing_tools(ttFonts, out=out, imgs=True)
     sys.exit(0)
 
 # get fonts before
-if args.path_before and not args.fetch_before:
+if args.path_before != "none" and args.fetch_before == "none":
     raise ValueError("--fetch-before flag required since path_before is provided")
 
 if args.fetch_before == "googlefonts":
     files_before = download_google_fonts_family(family_name, "files_before")
 elif args.fetch_before == "github-release":
-    if not args.repo:
+    if args.repo == "none":
         raise ValueError("--repo flag required e.g 'googlefonts/gulzar'")
     user, repo = args.repo.split("/")
     files_before = download_latest_github_release(user, repo, "files_before", args.github_token)
